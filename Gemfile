30b0e755076fa3c9d54070f7dc96227ad9e918c3
Karl Eklund <localpart@gmail.com>
2012-10-20 10:49:27 +0200

    Problem 12

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'http://rubygems.org'
 gem 'sinatra', '1.3.3'
 gem 'shotgun', '0.9'
 gem 'rspec', '2.11.0'
+gem 'nokogiri'
+
--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'json'
+require 'net/http'
+require 'nokogiri'
 
 get '/' do
   "Hello World"
@@ -9,8 +11,49 @@ end
 #   username == 'admin' && password == 'secret'
 # end
 
-get '/products.json' do
-  [ { name: "Foo", type: "Variable", price: 100 },
-    { name: "Bar", type: "Value", price: 200 },
-    { name: "Baz", type: "Function", price: 1 } ].to_json
+# Create a server which scrapes a newspaper for sections of text,
+# randomly select ten words > 5 character long and scramble them.
+
+# The scrambled words are sent to a client (in HTML/JS, for example),
+# which present them to a user, who try to guess the word, which are
+# sent back to the server and checked.
+
+# Client should count number of tries and switch words when correct
+# answer is found.
+
+get '/newspaper/words' do
+  headers 'Content-Type' => 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin' => '*'
+  page = Net::HTTP.get('www.dn.se', '/')
+  doc = Nokogiri::HTML(page)
+  paragraphs = doc.search('p').map {|para| para.text }
+  words = paragraphs.join(' ').split(' ').select { |w| w.length > 5 }.sort { |x,y| rand - 0.5 }.first(10)
+  scrambled = words.map {|word|
+    word.chars.sort { |x,y| rand - 0.5 }.join
+  }
+  wordfile = open('words.txt', 'w')
+  wordfile.write(words)
+  wordfile.close
+
+  scrambled.to_json
 end
+
+post '/newspaper/guess' do
+  content_type 'text/plain', charset: 'utf-8'
+  guess = params[:guess]
+  wordfile = open('words.txt')
+  words = JSON.parse(wordfile.read)
+  wordfile.close
+
+  if words.member? guess
+    status 200
+    "Right"
+  else
+    status 404
+    "Wrong"
+  end
+  # "The guess is #{guess}. " # + (words.member? guess ? "Correct!" : "Incorrect!")
+end
+
+
+# curl -v  --data "guess=hej" http://localhost:9393/newspaper/guess
